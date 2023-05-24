4226b4eb525442de8c6eaa442278146116aacbfb
Nathan DIERICKX <nath59310@gmail.com>
2023-05-24 15:46:03 +0200

    fix error message

--- password.js
+++ password.js
@@ -1,17 +1,16 @@
 export default function check_password(password) {
-    const errors = [];
-
+    let error;
     if (password.length < 8) {
-        errors.push("Le mot de passe doit comporter au moins 8 caractères");
+        error = "Le mot de passe doit comporter au moins 8 caractères";
     } else if ((password.match(/\d/g) || []).length < 2) {
-        errors.push("Le mot de passe doit contenir au moins 2 chiffres");
+        error = "Le mot de passe doit contenir au moins 2 chiffres";
     } else if (!/[A-Z]/.test(password)) {
-        errors.push("Le mot de passe doit contenir au moins une lettre majuscule");
+        error = "Le mot de passe doit contenir au moins une lettre majuscule";
     } else if (!/[\W_]/.test(password)) {
-        errors.push("Le mot de passe doit contenir au moins un caractère spécial");
+        error = "Le mot de passe doit contenir au moins un caractère spécial";
     } else {
-        errors.push("All checks are good");
+        error = "All checks are good";
     }
 
-    return errors;
+    return error;
 }
